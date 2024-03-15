1172c3840c6d4db8299d066fbd251df188e02e46
Dimantha Thiranjaya Weerakoon <dweerakoon71@gmail.com>
2024-03-15 23:29:12 +0530

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+import "swiper/css";
+import "./assets/boxicons-2.1.4/css/boxicons.min.css";
 import "./App.scss";
+
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return <div>App</div>;
